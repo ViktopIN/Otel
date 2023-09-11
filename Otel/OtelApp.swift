b5b5b99ec3a8ae712620dc0f9d5c5749d8efca57
Виктор Пинягин <viktor.piniagin@dzsystems.ru>
2023-09-11 14:58:08 +0300

    Initial Commit

---
+++ Otel/OtelApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtelApp.swift
+//  Otel
+//
+//  Created by Виктор on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct OtelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
