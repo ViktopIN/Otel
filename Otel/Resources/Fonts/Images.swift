a7677620159fd6a16961d475dedf68e8dd6c52ee
Виктор Пинягин <viktor.piniagin@dzsystems.ru>
2023-09-11 16:16:09 +0300

    Add images in project

---
+++ Otel/Resources/Fonts/Images.swift
@@ -0,0 +1,23 @@
+//
+//  Images.swift
+//  Otel
+//
+//  Created by Виктор on 11.09.2023.
+//
+
+import SwiftUI
+
+extension Image {
+    enum Icons {
+        static let chevronDown = Image("chevronDown")
+        static let chevronLeft = Image("chevronLeft")
+        static let chevronRight = Image("chevronRight")
+        static let chevronUp = Image("chevronUp")
+        static let closeSquare = Image("closeSquare")
+        static let emojiHappy = Image("emojiHappy")
+        static let plus = Image("plus")
+        static let star = Image("star")
+        static let tickSquare = Image("tickSquare")
+    }
+    static let partyPopper = Image("partyPopper")
+}
